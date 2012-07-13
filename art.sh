a9e6ff73db63643430c61566516bc6cddcddcb90
kafka399 <kafka399@gmail.com>
2012-07-13 23:15:49 +0200

    the end

--- art.sh
+++ art.sh
@@ -1,4 +1,4 @@
 RMDFILE=article
 Rscript -e "require(knitr); knit('$RMDFILE.Rmd', '$RMDFILE.md');"
-pandoc -s -S -i $RMDFILE.md -o $RMDFILE.html --self-contained
+pandoc -s -S -i $RMDFILE.md -o $RMDFILE.html 
 
