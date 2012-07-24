a48eac8d67cdc9ee3ce3a21f6484e6c1e4a0e1e7
kafka399 <kafka399@gmail.com>
2012-07-24 12:28:12 +0200

    presentation is ready to go

--- presentation.sh
+++ presentation.sh
@@ -1,3 +1,3 @@
 RMDFILE=presentation
 Rscript -e "require(knitr); knit('$RMDFILE.Rmd', '$RMDFILE.md');"
-pandoc -s -S -i -t dzslides $RMDFILE.md -o $RMDFILE.html --self-contained
+pandoc -s -S -i -t slidy $RMDFILE.md -o $RMDFILE.html --self-contained --mathjax
